8483bf1861db4d613f50564806b856aa73d0c06e
maghje13 <96776599+maghje13@users.noreply.github.com>
2024-01-10 20:55:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,46 @@ program_list = ["msf-payload-generator", "nmap-helper", "arp-discover", "RED_HAW
                 "subfinder", "wireshark", "brutal", "venom", "slowloris", "xsscon"]
 
 
+def show_description(program):
+    if program == "msf-payload-generator":
+        print("msf payload generator is a program created by me (maghje13)!\n"
+              "it uses metasploit-frameworks msfvenom command to create a backdoor payload\n"
+              "and can create a listener!\n"
+              "source: https://github.com/maghje13/payload-generator")
+    elif program == "nmap-helper":
+        print("nmap-helper is a program created by me (maghje13)!\n"
+              "it contains multiple presets for nmap scans\n"
+              "so you dont have to remember every single option :)")
+    elif program == "arp-discover":
+        print("arp discover is literally not even a program, just a command!\n"
+              "it does \"arp -a\" which shows all devices (and their mac addresses) it could find on your networks")
+    elif program == "RED_HAWK":
+        print("Redhawk is an advanced website recon and vulnerability scanner\n")
+
+
 def clear():
     os.system("clear")
+
+
+def show_options(program):
+    while True:
+        clear()
+        show_description(program)
+        menu_choice_2 = str(input("""
+Options:
+[1] Install
+[2] Run
+[3] Back
+
+> """))
+        if menu_choice_2 == "1":
+            install(program)
+        elif menu_choice_2 == "2":
+            run(program)
+        elif menu_choice_2 == "3":
+            break
+        else:
+            pass
 
 
 def install(program):
@@ -15,6 +53,9 @@ def install(program):
         os.system("cd programs/msf-payload-generator && bash install.sh")
     elif program == "nmap-helper":
         os.system("cd programs/nmap-helper && bash install.sh")
+    elif program == "arp-discover":
+        print("nothing to install for this...")
+        input("press enter to continue...")
     elif program == "RED_HAWK":
         os.system("sudo apt install php")
         os.system("cd programs && git clone git clone https://github.com/Tuhinshubhra/RED_HAWK.git")
@@ -54,6 +95,7 @@ def run(program):
         os.system("cd programs/nmap-helper && python3 main.py")
     elif program == "arp-discover":
         os.system("arp -a")
+        input("press enter to continue")
     elif program == "RED_HAWK":
         os.system("cd programs/red_hawk && php rhawk.php")
     elif program == "sqlmap":
@@ -85,76 +127,51 @@ def run(program):
 # main program
 while True:
     clear()
-    for line in open("ascii-art.txt", "r"):
-        print(line)
     menu_choice_1 = str(input("""Choose one of these options:
-    [1] msf payload generator
-    [2] nmap helper
-    [3] arp discover tool
-    [4] red hawk
-    [5] sqlmap
-    [6] setoolkit
-    [7] socialphish
-    [8] subfinder
-    [9] wireshark
-    [10] Brutal
-    [11] venom
-    [12] slowloris
-    [13] xsscon
-    [14] Exit
-    
-    > """))
-    if menu_choice_1 == "":
-        pass
-    elif menu_choice_1 == "14":
-        sys.exit()
-    elif menu_choice_1 == "1":
-        while True:
-            clear()
-            print("msf payload generator is a program created by me (maghje13)!\nit uses metasploit-framework's "
-                  "msfvenom command to create a backdoor payload, and can create a listener\n "
-                  "source: https://github.com/maghje13/payload-generator\n")
-            menu_choice_1 = str(input("""Options:
-[1] Install
-[2] Run
-[3] Exit
+[1] msf payload generator
+[2] nmap helper
+[3] arp discover tool
+[4] red hawk
+[5] sqlmap
+[6] setoolkit
+[7] socialphish
+[8] subfinder
+[9] wireshark
+[10] Brutal
+[11] venom
+[12] slowloris
+[13] xsscon
+[14] Exit
 
 > """))
-            if menu_choice_1 == "1":
-                install("msf-payload-generator")
-            elif menu_choice_1 == "2":
-                run("msf-payload-generator")
-            elif menu_choice_1 == "3":
-                break
-            else:
-                pass
+    if menu_choice_1 == "1":
+        show_options("msf-payload-generator")
+    elif menu_choice_1 == "2":
+        show_options("nmap-helper")
     elif menu_choice_1 == "3":
-        while True:
-            clear()
-            print("arp discover tool is a tool created by me (maghje13)!\n"
-                  "it uses the \"arp -a\" command to show all devices it can find on your network\n")
-            menu_choice_2 = str(input("""Options:
-[1] Run
-[2] Exit"""))
-            if menu_choice_2 == "1":
-                run("arp-discover")
-            elif menu_choice_2 == "2":
-                break
-            else:
-                pass
+        show_options("arp-discover")
     elif menu_choice_1 == "4":
-        while True:
-            clear()
-            print("Redhawk is a very good recon tool for scanning websites and finding vulnerabilities")
-            menu_choice_2 = str(input("""Options:
-        [1] Install
-        [2] Run
-        [3] Exit"""))
-            if menu_choice_2 == "1":
-                install("RED_HAWK")
-            elif menu_choice_2 == "2":
-                run("RED_HAWK")
-            elif menu_choice_2 == "3":
-                break
-            else:
-                pass
+        show_options("RED_HAWK")
+    elif menu_choice_1 == "5":
+        show_options("sqlmap")
+    elif menu_choice_1 == "6":
+        show_options("setoolkit")
+    elif menu_choice_1 == "7":
+        show_options("socialphish")
+    elif menu_choice_1 == "8":
+        show_options("subfinder")
+    elif menu_choice_1 == "9":
+        show_options("wireshark")
+    elif menu_choice_1 == "10":
+        show_options("brutal")
+    elif menu_choice_1 == "11":
+        show_options("venom")
+    elif menu_choice_1 == "12":
+        show_options("slowloris")
+    elif menu_choice_1 == "13":
+        show_options("xsscon")
+    elif menu_choice_1 == "14":
+        print("Exiting...")
+        sys.exit()
+    else:
+        pass
