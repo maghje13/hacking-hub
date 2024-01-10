2fc8ed51d91542fd72a7fceab24f303048884c48
maghje13 <96776599+maghje13@users.noreply.github.com>
2024-01-10 10:48:32 +0100

    Add files via upload

---
+++ programs/msf-payload-generator/install.sh
@@ -0,0 +1,7 @@
+echo Make sure to run this with sudo!
+echo Installing...
+mkdir payloads
+sudo apt install metasploit-framework
+sudo apt install python3
+sudo apt install pip
+pip install -r requirements.txt
